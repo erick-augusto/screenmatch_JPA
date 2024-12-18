46378e5b237aadac2eee8d444dd5479df9c88da5
erick-augusto <erick_aug@hotmail.com>
2024-12-18 13:29:27 -0300

    Salvando episódios no banco de dados

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,19 +1,12 @@
 package br.com.alura.screenmatch.model;
 
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 
 import br.com.alura.screenmatch.service.ConsultaMyMemory;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "series")
@@ -21,7 +14,6 @@ public class Serie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
@@ -30,12 +22,11 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
-    @Transient
+
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
-    public Serie(){
-    
-    }
+    public Serie() {}
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
@@ -47,72 +38,89 @@ public class Serie {
         this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
+        this.episodios = episodios;
+    }
+
     public String getTitulo() {
         return titulo;
     }
-    
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    
+
     public Integer getTotalTemporadas() {
         return totalTemporadas;
     }
-    
+
     public void setTotalTemporadas(Integer totalTemporadas) {
         this.totalTemporadas = totalTemporadas;
     }
-    
+
     public Double getAvaliacao() {
         return avaliacao;
     }
-    
+
     public void setAvaliacao(Double avaliacao) {
         this.avaliacao = avaliacao;
     }
-    
+
     public Categoria getGenero() {
         return genero;
     }
-    
+
     public void setGenero(Categoria genero) {
         this.genero = genero;
     }
-    
+
     public String getAtores() {
         return atores;
     }
-    
+
     public void setAtores(String atores) {
         this.atores = atores;
     }
-    
+
     public String getPoster() {
         return poster;
     }
-    
+
     public void setPoster(String poster) {
         this.poster = poster;
     }
-    
+
     public String getSinopse() {
         return sinopse;
     }
-    
+
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
     }
 
     @Override
     public String toString() {
-        return "Serie{" +
-           "genero=" + genero +
-           ", titulo='" + titulo + '\'' +
-           ", totalTemporadas=" + totalTemporadas +
-           ", avaliacao=" + avaliacao +
-           ", atores='" + atores + '\'' +
-           ", poster='" + poster + '\'' +
-           ", sinopse'" + sinopse + '\'' +
-           '}';
+        return
+                "genero=" + genero +
+                        ", titulo='" + titulo + '\'' +
+                        ", totalTemporadas=" + totalTemporadas +
+                        ", avaliacao=" + avaliacao +
+                        ", atores='" + atores + '\'' +
+                        ", poster='" + poster + '\'' +
+                        ", sinopse='" + sinopse + '\'' +
+                        ", episodios='" + episodios + '\'';
     }
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -10,13 +10,10 @@ import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class Principal {
 
@@ -26,12 +23,14 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
     private List<DadosSerie> dadosSeries = new ArrayList<>();
+
     private SerieRepository repositorio;
+    private List<Serie> series = new ArrayList<>();
 
     public Principal(SerieRepository repositorio) {
-        this.repositorio = repositorio; 
+        this.repositorio = repositorio;
     }
-    
+
     public void exibeMenu() {
         var opcao = -1;
         while(opcao != 0) {
@@ -67,7 +66,7 @@ public class Principal {
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
-        Serie serie = new Serie (dados);
+        Serie serie = new Serie(dados);
         //dadosSeries.add(dados);
         repositorio.save(serie);
         System.out.println(dados);
@@ -82,22 +81,40 @@ public class Principal {
     }
 
     private void buscarEpisodioPorSerie(){
-        DadosSerie dadosSerie = getDadosSerie();
-        List<DadosTemporada> temporadas = new ArrayList<>();
+        listarSeriesBuscadas();
+        System.out.println("Escolha uma série pelo nome");
+        var nomeSerie = leitura.nextLine();
 
-        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-            var json = consumo.obterDados(ENDERECO + dadosSerie.titulo().replace(" ", "+") + "&season=" + i + API_KEY);
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
+        Optional<Serie> serie = series.stream()
+            .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
+            .findFirst();
+
+        if(serie.isPresent()) {
+
+            var serieEncontrada = serie.get();
+            List<DadosTemporada> temporadas = new ArrayList<>();
+
+            for (int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++) {
+                var json = consumo.obterDados(ENDERECO + serieEncontrada.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
+                DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+                temporadas.add(dadosTemporada);
+            }
+            temporadas.forEach(System.out::println);
+
+            List<Episodio> episodios = temporadas.stream()
+                .flatMap(d -> d.episodios().stream()
+                .map(e -> new Episodio(d.numero(), e)))
+                .collect(Collectors.toList());
+
+            serieEncontrada.setEpisodios(episodios);
+            repositorio.save(serieEncontrada);
+        } else {
+            System.out.println("Série não encontrada!");
         }
-        temporadas.forEach(System.out::println);
     }
 
-    private void listarSeriesBuscadas() {
-        List<Serie> series = repositorio.findAll();
-        /*series = dadosSeries.stream()
-            .map(d -> new Serie(d))
-            .collect(Collectors.toList());*/
+    private void listarSeriesBuscadas(){
+        series = repositorio.findAll();
         series.stream()
             .sorted(Comparator.comparing(Serie::getGenero))
             .forEach(System.out::println);
